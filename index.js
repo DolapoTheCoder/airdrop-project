2453e520a32e6eb770740abf01a325188d8e5c84
DolapoTheCoder <65259627+DolapoTheCoder@users.noreply.github.com>
2022-08-06 14:11:47 +0100

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+const {
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL
+} = require('@solana/web3.js');
+
+
+//new wallet made via keyapir
+const wallet = new Keypair();
+
+//extract public and private key from wallet
+const publicKey = new PublicKey(wallet._keypair.publicKey);
+const secretKey = wallet._keypair.secretKey;
+
+console.log(publicKey);
+
+console.log(secretKey);
